2e268274218c3069ec3c779d5e8070eb0bc5d446
Jessica Janiuk <jessica.janiuk@gmail.com>
2013-01-10 13:41:34 -0700

    Started adding auth controllers

--- gophers/index.go
+++ gophers/index.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"gophers/controllers"
+	"gophers/controllers/admin"
+	"gophers/controllers/auth"
 	"gophers/plate"
 	"net/http"
 )
 
 func init() {
 	server := plate.NewServer("doughboy")
+	plate.DefaultAuthHandler = auth.AuthHandler
 
 	server.Get("/", controllers.Index)
+
+	//Admin Routes
+	server.Get("/Admin", admin.index).Secure()
 
 	session_key := "your key here"
 	http.Handle("/", server.NewSessionHandler(session_key, nil))
