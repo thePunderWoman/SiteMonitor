4487125375330292c46bd94c7637adee774f6fc1
Jessica Janiuk <jessica.janiuk@gmail.com>
2013-04-05 13:31:49 -0600

    Added capistrano deployment script

---
+++ config/deploy.rb
@@ -0,0 +1,47 @@
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+
+set :application, "GoAPI"
+set :repository,  "git@github.com:curt-labs/SiteMonitor.git"
+
+set :scm, :git
+set :scm_passphrase, ""
+set :user, "jjaniuk"
+
+role :web, "curt-api-server1.cloudapp.net", "curt-api-server2.cloudapp.net"
+role :app, "curt-api-server1.cloudapp.net", "curt-api-server2.cloudapp.net"
+
+set :deploy_to, "/home/jjaniuk/#{application}"
+set :deploy_via, :remote_cache
+
+set :use_sudo, false
+set :sudo_prompt, ""
+set :normalize_asset_timestamps, false
+
+after "deploy", "deploy:goget"
+after "deploy:goget", "deploy:compile"
+after "deploy:compile", "deploy:stop"
+after "deploy:stop", "deploy:restart"
+
+namespace :deploy do
+  task :goget do
+  	run "/usr/local/go/bin/go get github.com/ziutek/mymysql/native"
+  	run "/usr/local/go/bin/go get github.com/ziutek/mymysql/mysql"
+  end
+  task :compile do
+  	run "GOOS=linux GOARCH=amd64 CGO_ENABLED=0 /usr/local/go/bin/go build -o #{deploy_to}/current/site-monitor #{deploy_to}/current/index.go"
+  end
+  task :start do ; end
+  task :stop do 
+      kill_processes_matching "site-monitor"
+  end
+  task :restart do
+  	restart_cmd = "#{current_release}/site-monitor"
+  	run "nohup sh -c '#{restart_cmd} &' > nohup.out"
+  end
+end
+
+def kill_processes_matching(name)
+  run "ps -ef | grep #{name} | grep -v grep | awk '{print $2}' | sudo xargs kill -2 || echo 'no process with name #{name} found'"
+end
