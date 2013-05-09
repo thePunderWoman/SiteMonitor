f3858c8858405c1b65b5f3635b7dd6abc2132242
Jessica Janiuk <jessica.janiuk@gmail.com>
2013-05-09 17:07:23 +0000

    Added some logging to see whats happening on the server

--- models/history.go
+++ models/history.go
@@ -235,6 +235,7 @@ func Log(siteID int, checked time.Time, status string, emailed bool, code int, r
 
 func SaveLogs(logs []History) {
 	for i, _ := range logs {
+		log.Println(logs[i])
 		logs[i].Save()
 	}
 }
@@ -247,6 +248,7 @@ func (entry *History) Save() {
 	}
 
 	ins.Raw.Reset()
+	log.Println(entry)
 	ins.Bind(entry.SiteID, entry.Status, entry.Emailed, entry.Code, entry.ResponseTime)
 	_, _, err = ins.Exec()
 }
